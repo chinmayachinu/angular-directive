81033c5d6fefe2775411334e8c058a4b61f11495
chinmayachinu <chinmayapanigrahi.cp@gmail.com>
2021-04-06 16:26:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-directive';
+  currentStyles={
+    'background-color' : 'red' ,
+    'color' : 'green' 
+  }
+
+  val:string =''
+
+  listOfPlayer:string[] =['Chinmaya', 'Pinku', 'Abhisek'];
+
+  day:string=""
 }
